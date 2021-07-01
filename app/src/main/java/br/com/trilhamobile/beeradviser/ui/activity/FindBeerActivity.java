e43d6971ba20399949f70a16acb5fb71ffd59e8a
Denise Castro <dmaria.natal@gmail.com>
2021-07-01 08:02:24 -0300

    aplicativo concluído

--- app/src/main/java/br/com/trilhamobile/beeradviser/ui/activity/FindBeerActivity.java
+++ app/src/main/java/br/com/trilhamobile/beeradviser/ui/activity/FindBeerActivity.java
@@ -1,16 +1,39 @@
 package br.com.trilhamobile.beeradviser.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
 
 import br.com.trilhamobile.beeradviser.R;
 
-public class FindBeerActivity extends AppCompatActivity {
+public class FindBeerActivity extends Activity {
+    private BeerExpert expert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    //Chamado quando o botão é clicado
+    public void onClickFindBeer(View view) {
+        //Obtém ume referência para TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+        //Obtém uma referência para o Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+        //Obtém o item selecionado no Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+        //Obtém recomendações da classe BeerExpert
+        List<String> brandsList= expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand: brandsList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+        //Exibe as cervejas
+        brands.setText(brandsFormatted);
     }
 }
--- app/src/main/java/br/com/trilhamobile/beeradviser/ui/activity/MainActivity.java
+++ app/src/main/java/br/com/trilhamobile/beeradviser/ui/activity/MainActivity.java
@@ -2,13 +2,14 @@ package br.com.trilhamobile.beeradviser.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import br.com.trilhamobile.beeradviser.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onClickChoiceBeer(View view) {
+    public void choiceBeer(View view) {
         Intent intent = new Intent(this, FindBeerActivity.class);
         startActivity(intent);
         finish();
